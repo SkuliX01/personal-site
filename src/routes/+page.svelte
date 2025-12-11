847d0265a54d7b05bc606bf90502c1a3e00450d0
SkuliX01 <bkrawczyk@tutamail.com>
2025-12-11 16:20:37 +0100

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<style>
+@import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap');
+
+:global(body, html) {
+  font-family: "JetBrains Mono", sans-serif;
+  background-color: #f9f5d7;
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+</style>
+<div class="flex items-center justify-center mt-5">
+  <div class="w-[580px] border-2 p-2">
+    <div class="mr-auto">
+      <li class="list-none">
+        <a href="/blog" class="text-[#529295] hover:bg-[#529295] hover:text-gray-300 p-1">blog</a>
+        <a href="/projects" class="text-[#529295] hover:bg-[#529295] hover:text-gray-300 p-1">projects</a>
+      </li>
+      <div class="mt-2 bg-gray-500 h-px w-full"></div>
+    </div>
+    <div class="mt-10">
+      <h1 class="text-2xl font-bold">Hello Wanderer!</h1>
+      <p class="text-md ml-2">I want to welcome you to my own square of the <a href="https://en.wikipedia.org/wiki/World_Wide_Web" class="text-[#529295] underline underline-offset-2">World wide web</a>
+       . I'm 18 year old software developer, founder and Privacy Nomad from Poland. I begun coding in 2015 or 2016-I can't exactly remember I started from creating easy unity games from tutorials on YouTube. then in 2021 when COVID hit I was locked down at home, and thats the turing point where I really began to learn how to code.  
+      </p>
+    </div>
+  </div>
+</div>
